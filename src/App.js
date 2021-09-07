282b2eb12f1bdc20c200c3accfee214aa31ccf5e
Hjortur <hss44@hi.is>
2021-09-07 14:11:47 +0000

    Added states for numbers and operands

--- src/App.js
+++ src/App.js
@@ -1,49 +1,88 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { PriorityQueue } from './priorityQueue.js';
 
 const App = () => {
     const [result, setResult] = useState("");
+    const [num, setNum] = useState("");
+    const [nums, setNums] = useState([]);
+    const [ops, setOps] = useState([]);
+    
+    let pq = new PriorityQueue();
 
-    const handleClick = (e) => {
+    const handleNumClick = (e) => {
         setResult(result.concat(e.target.name));
+        setNum(num.concat(e.target.name));
+    }
+
+    const handleOpClick = (e) => {
+        setResult(result.concat(e.target.name));
+        setOps(ops => [...ops, e.target.name]);
+        setNums(nums => [...nums, num]);
+        setNum("");
     }
 
     const handleClear = () => {
         setResult("");
+        setNum("");
+        setNums([]);
+        setOps([]);
     }
 
     const handleBack = () => {
         setResult(result.slice(0, -1));
     }
 
-    const handleCalculate = () => {
-        setResult(eval(result)); // remove eval ...
+    const handleCalculate = (e) => {
+        setNums(nums => [...nums, num]);
+        setNum("");
+
+        calculate();
+    }
+
+    const calculate = () => {
+
+        // TODO: handle when a number is missing
+        // before or after an operand, also hanle
+        // when an operand is clicked right after
+        // some other operand had been clicked.
+
+        //setResult(result.replace("*", ""));
+        let a = result.search(/\*/);
+        let b = result.search(/\//);
+        let c = result.search(/\+/);
+        let d = result.search(/-/);
+     
+
+        // Clear the arrays after calculation has been done.
+        setNums([]);
+        setOps([]);
     }
 
     return (
-        <div className="container">
+        <div classvalue="container">
             <form>
-                <input type="text" value={result} />
+                <input type="text" value={result} readOnly />
             </form>
 
-            <div className="keypad">
+            <div classvalue="keypad">
                 <button onClick={handleClear}>Clear</button>
                 <button onClick={handleBack}>C</button>
-                <button name="/" onClick={handleClick}>&divide;</button>
-                <button name="7" onClick={handleClick}>7</button>
-                <button name="8" onClick={handleClick}>8</button>
-                <button name="9" onClick={handleClick}>9</button>
-                <button name="*" onClick={handleClick}>&times;</button>
-                <button name="4" onClick={handleClick}>4</button>
-                <button name="5" onClick={handleClick}>5</button>
-                <button name="6" onClick={handleClick}>6</button>
-                <button name="-" onClick={handleClick}>&ndash;</button>
-                <button name="1" onClick={handleClick}>1</button>
-                <button name="2" onClick={handleClick}>2</button>
-                <button name="3" onClick={handleClick}>3</button>
-                <button name="+" onClick={handleClick}>+</button>
-                <button name="0" onClick={handleClick}>0</button>
-                <button name="." onClick={handleClick}>.</button>
+                <button name="/" onClick={handleOpClick}>&divide;</button>
+                <button name="7" onClick={handleNumClick}>7</button>
+                <button name="8" onClick={handleNumClick}>8</button>
+                <button name="9" onClick={handleNumClick}>9</button>
+                <button name="*" onClick={handleOpClick}>&times;</button>
+                <button name="4" onClick={handleNumClick}>4</button>
+                <button name="5" onClick={handleNumClick}>5</button>
+                <button name="6" onClick={handleNumClick}>6</button>
+                <button name="-" onClick={handleOpClick}>&ndash;</button>
+                <button name="1" onClick={handleNumClick}>1</button>
+                <button name="2" onClick={handleNumClick}>2</button>
+                <button name="3" onClick={handleNumClick}>3</button>
+                <button name="+" onClick={handleOpClick}>+</button>
+                <button name="0" onClick={handleNumClick}>0</button>
+                <button name="." onClick={handleNumClick}>.</button>
                 <button name="=" onClick={handleCalculate}>=</button>                
             </div>
         </div>
